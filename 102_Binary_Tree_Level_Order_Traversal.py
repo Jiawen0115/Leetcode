4f69340540c519fec6079ebde98bee1f7cb7cd3c
Jiawen0115 <37093523+Jiawen0115@users.noreply.github.com>
2021-09-19 00:09:20 -0400

    Modify answers

--- 102_Binary_Tree_Level_Order_Traversal.py
+++ 102_Binary_Tree_Level_Order_Traversal.py
@@ -9,38 +9,23 @@
         #self.left = left
         #self.right = right
     
-class Solution(object):
-    def levelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         tree = []
         if not root:
             return tree
         if not root.left and not root.right:
             tree.append([root.val])
         else:
-            nodes = [root]
-            level = 0
-            while(nodes): #Pop nodes for every level, and add nodes in queue for next level
-						  #Every where a node is null, put None in queue 
-                level_values = []
-                if len(nodes) < 2**level+1:
-                    temp = nodes 
-                    nodes = []
-                else:
-                    temp = nodes[:2**level+1]
-                    del nodes[:2**level+1]
-                for item in temp:
-                    if item:
-                        level_values.append(item.val)
-                        nodes.extend([item.left,item.right])
-                    else:
-                        nodes.extend([None,None])
-                if not level_values:
-                    break
-                level += 1
-                tree.append(level_values)
+            nodes = [[root,0]]
+            while(nodes): 
+                node,level = nodes.pop(0)
+                if len(tree) <= level:
+                    tree.append([])
+                tree[level].append(node.val)
+                if node.left:
+                    nodes.append([node.left,level+1])
+                if node.right:
+                    nodes.append([node.right,level+1])
         return tree
                 
--- 387_First_Unique_Character_in_a_String.py
+++ 387_First_Unique_Character_in_a_String.py
@@ -2,29 +2,12 @@
 #			and return its index. If it does not exist, return -1.
 #Author: Jiawen Liu
 
-class Solution(object):
-    def firstUniqChar(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        visited = [0 for i in range(0,26)]
-        index_stack = []
-        val_stack = []
-        for i in range(0, len(s)):
-            if visited[ord(s[i]) - 97] == 0:
-                visited[ord(s[i]) - 97] = 1
-                index_stack.append(i)
-                val_stack.append(s[i])
-            else:
-                if s[i] in val_stack:
-                    index_stack.pop(val_stack.index(s[i]))
-                    val_stack.remove(s[i])
-        if len(val_stack) == len(s):
-            return 0
-        elif len(val_stack) == 0:
-            return -1
-        else:
-            return index_stack[0]
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        chars = {}
+        for i in range(0,len(s)):
+            chars[s[i]] = -1 if s[i] in chars else i
+        result = [chars[ch] for ch in chars if not chars[ch] == -1]
+        return result[0] if result else -1
                 
         
